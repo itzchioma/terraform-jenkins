3a06ba0ea16ed72d2bd3a735d358b75b1f5e0b8c
itzchioma <itzchioma10@gmail.com>
2024-04-16 14:05:46 -0500

    adding security group to the configuration file

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "public_instance" {
   instance_type = var.instance_type
   key_name      = var.key_name
   user_data     = file("userdata.tpl")
-  security_groups = ["aws_security_group.ec2_security_group"]
+  security_groups = ["aws_security_group.ec2_security_group.id"]
 
   tags = {
     Name = var.name_tag,
