e0dacbe018e75e6634f9b8e4bcaad371209d701e
itzchioma <itzchioma10@gmail.com>
2024-04-16 15:06:11 -0500

    adding security group to the configuration file

--- main.tf
+++ main.tf
@@ -4,7 +4,8 @@ resource "aws_instance" "public_instance" {
   instance_type = var.instance_type
   key_name      = var.key_name
   user_data     = file("userdata.tpl")
-  security_groups = ["${aws_security_group.ec2_security_group.id}"]
+  security_groups = ["sg-049596a95ad515c17"]
+  subnet_id = "subnet-0fc7476bed2680b57"
 
   tags = {
     Name = var.name_tag,
--- security_group.tf
+++ security_group.tf
@@ -1,40 +1,41 @@
-# Aws security group
-resource "aws_security_group" "ec2_security_group" {
-  name        = "security_group"
-  description = "allow access on port 8080 and 22"
-  vpc_id      = "vpc-0e511b44ed2a90b2b"
+# # Aws security group
+# resource "aws_security_group" "ec2_security_group" {
+#   name        = "security_group"
+#   description = "allow access on port 8080 and 22"
+#   vpc_id      = "vpc-0e511b44ed2a90b2b"
 
-  # allow access on port 8080
-  // Consolidated ingress block
-  ingress = [{
-    description      = "HTTP proxy access"
-    from_port        = 8080
-    to_port          = 8080
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = []
-    prefix_list_ids  = []
-    security_groups  = []
-    self             = false
-    },
-    # allow access on port 22
-    {
-      description      = "ssh access"
-      from_port        = 22
-      to_port          = 22
-      protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = []
-      prefix_list_ids  = []
-      security_groups  = []
-      self             = false
-  }]
+#   # allow access on port 8080
+#   // Consolidated ingress block
+#   ingress = [{
+#     description      = "HTTP proxy access"
+#     from_port        = 8080
+#     to_port          = 8080
+#     protocol         = "tcp"
+#     cidr_blocks      = ["0.0.0.0/0"]
+#     ipv6_cidr_blocks = []
+#     prefix_list_ids  = []
+#     security_groups  = []
+#     self             = false
+#     },
+#     # allow access on port 22
+#     {
+#       description      = "ssh access"
+#       from_port        = 22
+#       to_port          = 22
+#       protocol         = "tcp"
+#       cidr_blocks      = ["0.0.0.0/0"]
+#       ipv6_cidr_blocks = []
+#       prefix_list_ids  = []
+#       security_groups  = []
+#       self             = false
+#   }]
 
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-}
+#   egress {
+#     from_port        = 0
+#     to_port          = 0
+#     protocol         = "-1"
+#     cidr_blocks      = ["0.0.0.0/0"]
+#     ipv6_cidr_blocks = ["::/0"]
+#   }
+# }
+
