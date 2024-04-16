139e51bed973528a6d0cb2e1872d6d5aff45d309
itzchioma <itzchioma10@gmail.com>
2024-04-16 14:25:06 -0500

    adding security group to the configuration file

--- security_group.tf
+++ security_group.tf
@@ -1,6 +1,6 @@
 # Aws security group
 resource "aws_security_group" "ec2_security_group" {
-  name        = "ec2_security_group"
+  name        = "security_group"
   description = "allow access on port 8080 and 22"
   vpc_id      = "vpc-0e511b44ed2a90b2b"
 
